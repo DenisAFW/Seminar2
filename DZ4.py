0ef75030081df64895fdeb5884f1a22dd4154cb4
Choppa <denis.afinogenow@yandex.ru>
2022-09-12 20:29:05 +0300

    списал ыгы

--- DZ4.py
+++ DZ4.py
@@ -11,3 +11,20 @@ for i in range(N):
     
 print(numbers)
 print(numbers[1] * numbers[3])
+
+f = open('file.txt', 'w')
+while True:
+    s = input('Укажите позицию для вычисления - ')
+    if s == "":
+        break
+    f.write(s+"\n")
+f.close()
+
+result = 1
+f = open('file.txt', 'r')
+for line in f:
+    if line == "":
+        break
+    result *= numbers[int(line)]
+f.close()
+print(result)
